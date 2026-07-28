202b8c97f29fc9e761d9e8e06227b4b77dea73f6
kwpark0047 <kwpark0047@gmail.com>
2026-07-28 22:21:39 +0900

    fix: Express 5 - skip req.query/params sanitization (read-only getters)

--- middleware/xssSanitizer.js
+++ middleware/xssSanitizer.js
@@ -134,19 +134,8 @@ const strictSanitizer = (req, res, next) => {
   if (req.body && typeof req.body === 'object') {
     req.body = sanitizeObject(req.body);
   }
-  if (req.query && typeof req.query === 'object') {
-    // Express 5: req.query is read-only, create sanitized copy
-    const sanitizedQuery = sanitizeObject(req.query);
-    Object.keys(req.query).forEach(key => delete req.query[key]);
-    Object.assign(req.query, sanitizedQuery);
-  }
-  if (req.params && typeof req.params === 'object') {
-    // Express 5: req.params is read-only
-    const sanitizedParams = sanitizeObject(req.params);
-    Object.keys(req.params).forEach(key => delete req.params[key]);
-    Object.assign(req.params, sanitizedParams);
-  }
-
+  // Express 5: req.query and req.params are read-only getters, skip sanitization
+  // Individual routes can sanitize query params as needed
   next();
 };
 
